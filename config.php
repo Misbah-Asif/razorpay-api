eb3880f0c7df1c0f627b0d92829bc91508c9770e
Misbah <misbahasif37@gmail.com>
2021-05-16 19:36:34 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_test_Bgg7SWZN2fRY3s';
+$keySecret = 'ouKKINNDD2UyJrEBLK4xJxcz';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
